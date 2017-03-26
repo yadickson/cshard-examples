f1d46fe77248c0eaf8bfe4281957324226dccd17
Yadickson Soto <yadickson.soto@basedos.cl>
2017-03-26 02:02:17 -0300

    Se cambian los nombres de metodos de los SP

--- IntegracionWD/Constants/Business.cs
+++ IntegracionWD/Constants/Business.cs
@@ -13,14 +13,14 @@ namespace IntegracionWD.Constants
         public const string SERVICIO_TRANSITO = "WS-ST";
         public const string SERVICIO_IDENTIFICADOR = "WS-SI";
 
-        public const string SP_PERSONAS = "SP_INSERTAR_PERSONAS";
-        public const string SP_VEHICULOS = "SP_INSERTAR_VEHICULOS";
-        public const string SP_TRANSITO = "SP_CONSULTA_TRANSITO";
-        public const string SP_IDENTIFICADOR = "SP_CONSULTA_IDENTIFICADOR";
+        public const string SP_PERSONAS = "InsertarPersona";
+        public const string SP_VEHICULOS = "InsertarVehiculo";
+        public const string SP_TRANSITO = "ConsultaTransito";
+        public const string SP_IDENTIFICADOR = "ConsultaIdentificador";
 
-        public const string SP_LOG_PERSONAS = "SP_INSERTAR_LOG_PERSONAS";
-        public const string SP_LOG_VEHICULOS = "SP_INSERTAR_LOG_VEHICULOS";
-        public const string SP_LOG_TRANSITO = "SP_INSERTAR_LOG_CONSULTA_TRANSITO";
-        public const string SP_LOG_IDENTIFICADOR = "SP_INSERTAR_LOG_CONSULTA_IDENTIFICADOR";
+        public const string SP_LOG_PERSONAS = "LogPersona";
+        public const string SP_LOG_VEHICULOS = "LogVehiculo";
+        public const string SP_LOG_TRANSITO = "LogTransito";
+        public const string SP_LOG_IDENTIFICADOR = "LogIdentificador";
     }
 }
