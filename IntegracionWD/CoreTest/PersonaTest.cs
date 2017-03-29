76836d0e9bb2f38163efbfcdba813e728a30454a
Yadickson Soto <yadickson@gmail.com>
2017-03-29 15:00:56 -0400

    Incorporacion de pruebas unitarias para el core de personas

---
+++ IntegracionWD/CoreTest/PersonaTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntegracionWD.Exception;
+using IntegracionWD.Core;
+using IntegracionWD.Domain;
+using IntegracionWD.DataBase;
+using IntegracionWD.Constants;
+using Moq;
+
+namespace CoreTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private PersonaInterface persona;
+        private Mock<ValidarDataInterface<DataPersona>> validador;
+        private Mock<LoggerDaoInterface> loggerDao;
+        private Mock<PersonaDaoInterface> personaDao;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            validador = new Mock<ValidarDataInterface<DataPersona>>();
+            loggerDao = new Mock<LoggerDaoInterface>();
+            personaDao = new Mock<PersonaDaoInterface>();
+            persona = new PersonaImpl(validador.Object, personaDao.Object, loggerDao.Object);
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAgregarPersonaFallaValidacion()
+        {
+            validador.Setup(v => v.Validar(It.IsAny<DataPersona>())).Throws(new BusinessException("Validador error", "0001"));
+            loggerDao.Setup(l => l.Agregar(It.IsAny<string>(), It.IsAny<string>())).Verifiable();
+
+            Respuesta response = persona.AgregarPersona(new DataPersona());
+
+            validador.Verify(v => v.Validar(It.IsAny<DataPersona>()), Times.Once());
+            loggerDao.Verify(l => l.Agregar(It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+            personaDao.Verify(p => p.AgregarPersona(It.IsAny<DataPersona>()), Times.Never());
+
+            Assert.IsNotNull(response);
+
+            string cExpected = Messages.CODIGO_RECHAZADO;
+            string mExpected = "Validador error";
+            string csExpected = Business.SERVICIO_PERSONAS + "0001";
+
+            Assert.AreEqual(cExpected, response.Codigo);
+            Assert.AreEqual(mExpected, response.Mensaje);
+            Assert.AreEqual(csExpected, response.CodigoServicio);
+
+            validador.VerifyAll();
+            loggerDao.VerifyAll();
+            personaDao.VerifyAll();
+        }
+
+        [TestMethod]
+        public void TestAgregarPersonaFallaDao()
+        {
+            validador.Setup(v => v.Validar(It.IsAny<DataPersona>())).Returns(new DataPersona());
+            loggerDao.Setup(l => l.Agregar(It.IsAny<string>(), It.IsAny<string>())).Verifiable();
+            personaDao.Setup(p => p.AgregarPersona(It.IsAny<DataPersona>())).Throws(new BusinessException("Dao error", "0002"));
+
+            Respuesta response = persona.AgregarPersona(new DataPersona());
+
+            validador.Verify(v => v.Validar(It.IsAny<DataPersona>()), Times.Once());
+            loggerDao.Verify(l => l.Agregar(It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+            personaDao.Verify(p => p.AgregarPersona(It.IsAny<DataPersona>()), Times.Once());
+
+            Assert.IsNotNull(response);
+
+            string cExpected = Messages.CODIGO_RECHAZADO;
+            string mExpected = "Dao error";
+            string csExpected = Business.SERVICIO_PERSONAS + "0002";
+
+            Assert.AreEqual(cExpected, response.Codigo);
+            Assert.AreEqual(mExpected, response.Mensaje);
+            Assert.AreEqual(csExpected, response.CodigoServicio);
+
+            validador.VerifyAll();
+            loggerDao.VerifyAll();
+            personaDao.VerifyAll();
+        }
+
+        [TestMethod]
+        public void TestAgregarPersonaCorrecto()
+        {
+            Respuesta daoResponse = new Respuesta();
+            daoResponse.Codigo = "C";
+            daoResponse.Mensaje = "M";
+            daoResponse.CodigoServicio = "CS";
+
+            validador.Setup(v => v.Validar(It.IsAny<DataPersona>())).Returns(new DataPersona());
+            personaDao.Setup(p => p.AgregarPersona(It.IsAny<DataPersona>())).Returns(daoResponse);
+
+            Respuesta response = persona.AgregarPersona(new DataPersona());
+
+            validador.Verify(v => v.Validar(It.IsAny<DataPersona>()), Times.Once());
+            loggerDao.Verify(l => l.Agregar(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+            personaDao.Verify(p => p.AgregarPersona(It.IsAny<DataPersona>()), Times.Once());
+
+            Assert.IsNotNull(response);
+
+            string cExpected = "C";
+            string mExpected = "M";
+            string csExpected = "CS";
+
+            Assert.AreEqual(cExpected, response.Codigo);
+            Assert.AreEqual(mExpected, response.Mensaje);
+            Assert.AreEqual(csExpected, response.CodigoServicio);
+
+            validador.VerifyAll();
+            loggerDao.VerifyAll();
+            personaDao.VerifyAll();
+        }
+    }
+}
